703b2f57a832ea1e12e4cd14035537678ff4c6ad
SATYAM SINGH <141752694+Ssingh0101@users.noreply.github.com>
2023-08-28 02:19:43 +0530

    main.js

---
+++ main.js
@@ -0,0 +1,25 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const jokeForm = document.getElementById("jokeForm");
+    const jokeDisplay = document.getElementById("jokeDisplay");
+
+    jokeForm.addEventListener("submit", async (event) => {
+        event.preventDefault();
+        
+        const selectedCategory = document.getElementById("category").value;
+        const apiUrl = `https://v2.jokeapi.dev/joke/${selectedCategory}`;
+
+        try {
+            const response = await fetch(apiUrl);
+            const data = await response.json();
+
+            if (data.joke) {
+                jokeDisplay.innerHTML = `<p>${data.joke}</p>`;
+            } else {
+                jokeDisplay.innerHTML = "<p>No joke available for this category.</p>";
+            }
+        } catch (error) {
+            jokeDisplay.innerHTML = "<p>An error occurred while fetching the joke.</p>";
+            console.error(error);
+        }
+    });
+});s
